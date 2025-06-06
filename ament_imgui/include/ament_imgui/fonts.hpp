af90b327909942fd5bdc71362c89cded0d827103
Pepe Ojeda <ojedamorala@hotmail.com>
2025-06-06 14:23:14 +0200

    added embedded fonts

--- ament_imgui/include/ament_imgui/fonts.hpp
+++ ament_imgui/include/ament_imgui/fonts.hpp
@@ -4,6 +4,12 @@
 
 namespace ImGui::Fonts
 {
-    extern const uint8_t Roboto_VariableFont[];
-    extern const size_t Roboto_VariableFont_size;
+    extern const uint8_t Roboto_Variable[];
+    extern const size_t Roboto_Variable_size;
+
+    extern const uint8_t ClimateCrisis_Variable[];
+    extern const size_t ClimateCrisis_Variable_size;
+
+    extern const uint8_t Revalia_Regular[];
+    extern const size_t Revalia_Regular_size;
 }
