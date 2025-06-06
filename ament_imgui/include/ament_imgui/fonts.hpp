7cf4dcf90c83125336a3f528fb8b6f72625c350f
Pepe Ojeda <ojedamorala@hotmail.com>
2025-06-06 09:57:16 +0200

    added embedded roboto font

---
+++ ament_imgui/include/ament_imgui/fonts.hpp
@@ -0,0 +1,9 @@
+// array size is 468308
+#include <cstddef>
+#include <cstdint>
+
+namespace ImGui::Fonts
+{
+    extern const uint8_t Roboto_VariableFont[];
+    extern const size_t Roboto_VariableFont_size;
+}
