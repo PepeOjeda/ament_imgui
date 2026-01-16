207ba1a396f8eb69ea654536fe7e67335cbbf407
PepeOjeda <ojedamorala@hotmail.com>
2026-01-16 13:47:56 +0100

    changed all functions and class members to be static

--- imgui_gl/include/imgui_gl/imgui_gl.h
+++ imgui_gl/include/imgui_gl/imgui_gl.h
@@ -7,21 +7,21 @@ struct GLFWwindow;
 class ImguiGL
 {
 public:
-    void Setup(const char* ini_file_path = nullptr,
+    static void Setup(const char* ini_file_path = nullptr,
                const char* window_title = "imgui_window",
                float width = 550,
                float height = 600,
                ImGuiConfigFlags flags = ImGuiConfigFlags_NavEnableKeyboard | ImGuiConfigFlags_DockingEnable);
-    void Close();
-    void StartFrame();
-    void Render();
+    static void Close();
+    static void StartFrame();
+    static void Render();
 
-    constexpr ImGuiConfigFlags FlagsFixedLayout()
+    static constexpr ImGuiConfigFlags FlagsFixedLayout()
     {
         return ImGuiWindowFlags_NoCollapse | ImGuiWindowFlags_NoResize | ImGuiWindowFlags_NoMove;
     }
 
-    void SetNextWindowFullscreen()
+    static void SetNextWindowFullscreen()
     {
         ImGuiIO& io = ImGui::GetIO();
         ImGui::SetNextWindowSize({io.DisplaySize.x, io.DisplaySize.y});
@@ -31,13 +31,13 @@ public:
         ImGui::SetNextWindowPos(ImVec2(main_viewport->WorkPos.x, main_viewport->WorkPos.y));
     }
 
-    bool ShouldClose();
+    static bool ShouldClose();
 
 public:
-    GLFWwindow* window;
-    ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
+    static inline GLFWwindow* window;
+    static inline ImVec4 clear_color = ImVec4(0.45f, 0.55f, 0.60f, 1.00f);
 
 private:
-    std::string window_title_str;
-    std::string filepath;
+    static inline std::string window_title_str;
+    static inline std::string filepath;
 };
